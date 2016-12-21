eca64e0876ab858ac00aa507368bd488a7f9c822
Administrator <licavins@gmail.com>
2016-12-21 10:04:19 +0800

    协作者测试修改

--- aios-bridge/src/main/java/com/aispeech/aios/bridge/activity/MainActivity.java
+++ aios-bridge/src/main/java/com/aispeech/aios/bridge/activity/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        //其他账户push成功
 //        ListView mListView = (ListView) findViewById(R.id.list_view);
 //        String versionText = String.format(getString(R.string.sdk_version), SDKBuild.VERSION.NAME);
 //        String versionText = "欢迎使用AIOS for car SDK";
